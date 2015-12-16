1e509afab1bbe50a3cd64ca87fab66052d3a572c
Martin Stannard <martin@stannard.id.au>
2015-12-16 21:26:42 +1100

    sublist again

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -3,38 +3,35 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) do
-
-    if length(a) > length(b) do
-      do_compare(b, a, :superlist)
-    else
-      do_compare(a, b, :sublist)
+  def compare(list1, list2) do
+    # compare length reduce to 1 sort of compare, pass in list type
+    cond do
+      length(list1) < length(list2) ->
+        do_compare(list1, list2, :sublist)
+      length(list1) == length(list2) ->
+        do_compare(list2, list1, :equal)
+      length(list1) > length(list2) ->
+        do_compare(list2, list1, :superlist)
     end
   end
 
-  defp do_compare(a, b, sub) do
-    case subby(a, b) do
-      true ->
-        if length(a) == length(b) do
-          :equal
-        else
-          sub
-        end
-      false ->
-        :unequal
+  defp do_compare(list1, list2, subtype) do
+    case subby(list1, list2) do
+      true -> subtype
+      false -> :unequal
     end
   end
 
-  defp subby(a, b), do: is_sub(a, b, a, b)
+  defp subby(list1, list2), do: is_sub(list1, list2, list1, list2)
 
-  def is_sub([], _, _, _), do: true
-  def is_sub(_, [], _, _), do: false
-  def is_sub([h1|t1], [h2|t2], list1, [hl2|tl2]) do
+  defp is_sub([], _, _, _), do: true
+  defp is_sub(_, [], _, _), do: false
+  defp is_sub([h1|t1], [h2|t2], list1, [hl2|tl2]) do
     case h1 === h2 do
       true -> is_sub(t1, t2, list1, [hl2|tl2]) 
       _ -> is_sub(list1, tl2, list1, [h2|t2])
     end
   end
-  def is_sub(a, [h|t], _, _), do: a === h
+  defp is_sub(a, [h|_], _, _), do: a === h
 
 end
